72b95925e710da5a1b0160d9786dc5e61782d82e
Anoop Naravaram <anoop901@gmail.com>
2023-10-27 21:03:13 -0700

    functions to rotate sticker locations

--- src/StickerLocation.ts
+++ src/StickerLocation.ts
@@ -1,5 +1,4 @@
-import * as THREE from "three";
-import { CubeSide, cubeSideToMatrix4 } from "./CubeSide";
+import { CubeSide } from "./CubeSide";
 
 export type StickerLocation = [number, number, number];
 
@@ -7,16 +6,63 @@ export function stickerLocationsOnCubeSide(
   cubeSide: CubeSide
 ): StickerLocation[] {
   const stickerLocations = [];
-  const rotationMatrix = cubeSideToMatrix4(cubeSide);
+  const [cubeSideX, cubeSideY, cubeSideZ] = cubeSide;
   for (let x = -1; x <= 1; x++) {
     for (let y = -1; y <= 1; y++) {
-      const frontStickerLocation = [x, y, 2];
-      const stickerLocation = new THREE.Vector3()
-        .fromArray(frontStickerLocation)
-        .applyMatrix4(rotationMatrix)
-        .toArray() as StickerLocation;
+      const frontStickerLocation = [x, y, 2] as StickerLocation;
+      const stickerLocation =
+        cubeSideX > 0
+          ? rotateStickerLocationRight(frontStickerLocation)
+          : cubeSideX < 0
+          ? rotateStickerLocationLeft(frontStickerLocation)
+          : cubeSideY > 0
+          ? rotateStickerLocationUp(frontStickerLocation)
+          : cubeSideY < 0
+          ? rotateStickerLocationDown(frontStickerLocation)
+          : cubeSideZ < 0
+          ? rotateStickerLocationRight(
+              rotateStickerLocationRight(frontStickerLocation)
+            )
+          : frontStickerLocation;
       stickerLocations.push(stickerLocation);
     }
   }
   return stickerLocations;
+}
+
+export function rotateStickerLocationDown(
+  stickerLocation: StickerLocation
+): StickerLocation {
+  const [x, y, z] = stickerLocation;
+  return [x, -z, y];
+}
+export function rotateStickerLocationUp(
+  stickerLocation: StickerLocation
+): StickerLocation {
+  const [x, y, z] = stickerLocation;
+  return [x, z, -y];
+}
+export function rotateStickerLocationRight(
+  stickerLocation: StickerLocation
+): StickerLocation {
+  const [x, y, z] = stickerLocation;
+  return [z, y, -x];
+}
+export function rotateStickerLocationLeft(
+  stickerLocation: StickerLocation
+): StickerLocation {
+  const [x, y, z] = stickerLocation;
+  return [-z, y, x];
+}
+export function rotateStickerLocationCounterClockwise(
+  stickerLocation: StickerLocation
+): StickerLocation {
+  const [x, y, z] = stickerLocation;
+  return [y, -x, z];
+}
+export function rotateStickerLocationClockwise(
+  stickerLocation: StickerLocation
+): StickerLocation {
+  const [x, y, z] = stickerLocation;
+  return [-y, x, z];
 }
